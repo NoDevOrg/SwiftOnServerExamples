f97f86637a5bbbe85afa09027b2a69d88eba7e9c
Sami Suteria <sami.suteria@gmail.com>
2021-08-21 19:34:28 -0500

    configure server

--- Persistence/Sources/Server/Server.swift
+++ Persistence/Sources/Server/Server.swift
@@ -1,11 +1,21 @@
 import Vapor
+import Fluent
+import FluentPostgresDriver
+
+enum ApplicationError: Error {
+    case missingDatabaseURL
+}
 
 public func application() throws -> Application {
     var environment = try Environment.detect()
     try LoggingSystem.bootstrap(from: &environment)
     let application = Application(environment)
     
-    // Insert additional configuration here
+    // Configure Database
+    guard let databaseURL = Environment.get("DATABASE_URL") else {
+        throw ApplicationError.missingDatabaseURL
+    }
     
+    try application.databases.use(.postgres(url: databaseURL), as: .psql)
     return application
 }
