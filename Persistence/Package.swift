134932d73d22548cb7d410909d1a40fcec400cf3
Sami Suteria <sami.suteria@gmail.com>
2021-08-21 19:34:19 -0500

    add packages

--- Persistence/Package.swift
+++ Persistence/Package.swift
@@ -8,12 +8,16 @@ package.platforms = [
 ]
 
 package.dependencies = [
-    .package(url: "https://github.com/vapor/vapor", .upToNextMajor(from: "4.0.0"))
+    .package(url: "https://github.com/vapor/vapor", .upToNextMajor(from: "4.0.0")),
+    .package(url: "https://github.com/vapor/fluent.git", from: "4.0.0"),
+    .package(url: "https://github.com/vapor/fluent-postgres-driver.git", from: "2.0.0"),
 ]
 
 package.targets = [
     .target(name: "Server", dependencies: [
-        .product(name: "Vapor", package: "vapor")
+        .product(name: "Vapor", package: "vapor"),
+        .product(name: "Fluent", package: "fluent"),
+        .product(name: "FluentPostgresDriver", package: "fluent-postgres-driver")
     ]),
     .target(name: "Run", dependencies: [
         .target(name: "Server")
